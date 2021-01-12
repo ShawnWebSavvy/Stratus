9d71b22c346d8d3816375ed9003e4549349390cf
rowsheet <rowsheet.com@gmail.com>
2021-01-12 11:40:47 -0700

    Credentials are in the .htaccess file. Don't commit this. TODO: Put this in the README.md

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,24 @@
 <?php
-define("DB_NAME", "sngine");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_HOST", "localhost");
-define("DB_PORT", "3306");
-define("SYS_URL", "http://localhost/sngine");
-define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
+/*
+
+Note: You need to set these in the .htaccess file per whatever
+the environment sepcifications are.
+
+SetEnv DB_NAME some_database
+SetEnv DB_USER some_user
+SetEnv DB_PASSWORD some_password 
+SetEnv DB_HOST some_host
+SetEnv DB_PORT some_port
+SetEnv SYS_URL some_url
+SetEnv API_BASE_URL some_url
+ */
+define("DB_NAME", getenv("DB_NAME"));
+define("DB_USER", getenv("DB_USER"));
+define("DB_PASSWORD", getenv("DB_PASSWORD"));
+define("DB_HOST", getenv("DB_HOST"));
+define("DB_PORT", getenv("DB_PORT"));
+define("SYS_URL", getenv("SYS_URL"));
+define("API_BASE_URL", getenv("API_BASE_URL"));
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
