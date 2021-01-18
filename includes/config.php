505954c142b064e6ebd66a604d3287635551cc2b
Karan Kumar <karan.kumar@antiersolution.com>
2021-01-18 09:55:52 +0530

    Updated Branch

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ define("DB_USER", "root");
 define("DB_PASSWORD", "");
 define("DB_HOST", "localhost");
 define("DB_PORT", "3306");
-define("SYS_URL", "http://localhost/stratus");
+define("SYS_URL", "http://localhost/stratus_speed");
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
