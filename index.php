2e7a7aaa1866324bf74e204239b9a51623354fa7
Karan Kumar <karan.kumar@antiersolution.com>
2021-02-09 18:50:03 +0530

    Redis Udated

--- index.php
+++ index.php
@@ -28,6 +28,13 @@ try {
 		$smarty->assign('custom_fields', $user->get_custom_fields());
 	} else {
 		$redisObject = new RedisClass();
+		$array = $redisObject->getStoredKeys();
+		// echo "<pre>";
+		// print_r($array);
+		// foreach ($array as $key) {
+		// 	$redisObject->deleteValueFromKey($key);
+		// }
+		// die;
 
 		// user access
 		user_access();
