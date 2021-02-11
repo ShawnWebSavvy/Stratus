14828f396926971d812105a45d99f28c1ff5d75b
Karan Kumar <karan.kumar@antiersolution.com>
2021-02-11 18:17:23 +0530

    Updaetd Bugs

--- includes/config.php
+++ includes/config.php
@@ -1,23 +1,11 @@
 <?php
-$is_secure = false;
-if (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') {
-    $is_secure = true;
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
-    $is_secure = true;
-}
-$protocol = $is_secure ? 'https' : 'http';
-$system_url =  $protocol . "://" . $_SERVER['HTTP_HOST'];
-define("DB_NAME", "s2_stratus");
-define("DB_USER", "s2_stratu");
-define("DB_PASSWORD", "cs3gnMHw");
-define("DB_HOST", "10.1.0.16");
+define("DB_NAME", "stratus");
+define("DB_USER", "root");
+define("DB_PASSWORD", "Admin@123");
+define("DB_HOST", "localhost");
 define("DB_PORT", "3306");
-define("SYS_URL", "http://localhost/stratus");
+define("SYS_URL", "http://localhost/redis");
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
-define("PLY_URL", "http://localhost/playtube");
-
-
-define("PLAYTUBE_LINK", "http://localhost/playtube/logout");
