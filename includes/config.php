fac5e4f92ff7154ff318995d0c2eb5001bbbc93f
kamaljit-antech <kamaljit.kaur@antechindia.com>
2021-02-12 18:15:34 +0530

    qa bugs fixes

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,13 @@
 <?php
-define("DB_NAME", "stratus");
-define("DB_USER", "root");
-define("DB_PASSWORD", "Admin@123");
-define("DB_HOST", "localhost");
+define("DB_NAME", "s2_stratus");
+define("DB_USER", "s2_stratu");
+define("DB_PASSWORD", "cs3gnMHw");
+define("DB_HOST", "10.1.0.16");
 define("DB_PORT", "3306");
-define("SYS_URL", "http://localhost/redis");
-define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
+define("SYS_URL", "http://localhost/stratus");
+// define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
+define("API_BASE_URL", "https://ws.knoxglobal.com/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
+define("PLY_URL", "http://localhost/playtube");
