5b7b5130769cbffc7eabf91158c0f07b91b7911d
Ankit Sharma <anit.sharma2@antiersolutions.com>
2021-03-03 15:11:50 +0530

    Worked on wallet sync with videohub

--- web_apis/wallet_apis.php
+++ web_apis/wallet_apis.php
@@ -16,15 +16,21 @@ function getWalletBalance($token){
         returnResponse(false,402,"Invalid access token provided");
       }else{
 
-        if(isset($_POST['id'])){
+
+      //  print_r($_POST); die;
+
+        if(isset($_POST['email'])){
           // $check_user = $db->query(sprintf("SELECT COUNT(*) as count FROM users WHERE user_id = %s", secure($_POST['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
-          $check_user = $db->query(sprintf("SELECT COUNT(*) as count FROM users WHERE user_id = %s", secure($_POST['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
+          // $check_user = $db->query(sprintf("SELECT COUNT(*) as count FROM users WHERE user_id = %s", secure($_POST['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
+            $check_user = $db->query(sprintf("SELECT COUNT(*) as count FROM users WHERE user_email = %s", secure($_POST['email']))) or _error("SQL_ERROR_THROWEN");
           $check_user= $check_user->fetch_assoc();
           if($check_user['count'] < 1){
             returnResponse(false,402,"Invalid user");
 
           }else{
-            $wallet_query = $db->query(sprintf("SELECT  * FROM users WHERE user_id = %s", secure($_POST['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
+            // $wallet_query = $db->query(sprintf("SELECT  * FROM users WHERE user_id = %s", secure($_POST['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
+            $wallet_query = $db->query(sprintf("SELECT  * FROM users WHERE user_email = %s", secure(  $_POST['email']))) or _error("SQL_ERROR_THROWEN");
+
             $result= $wallet_query->fetch_assoc();
             if(!empty($result)){
               returnResponse(true,200,"Success",$result);
@@ -64,19 +70,22 @@ function updateWAlletBalanceFunction($token){
           returnResponse(false,402,"Invalid access token provided");
         }else{
 
-            if(isset($_POST['id']) && isset($_POST['price'])){
-              $check_user = $db->query(sprintf("SELECT COUNT(*) as count FROM users WHERE user_id = %s", secure($_POST['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
+            if(isset($_POST['email']) && isset($_POST['price'])){
+              $check_user = $db->query(sprintf("SELECT user_id as id, COUNT(*) as count FROM users WHERE user_email = %s", secure($_POST['email']))) or _error("SQL_ERROR_THROWEN");
               $check_user= $check_user->fetch_assoc();
+
+            //  print_r($check_user); die;
+
               if($check_user['count'] < 1){
                 returnResponse(false,402,"Invalid user");
 
               }else{
-                $query =   $db->query(sprintf('UPDATE users SET user_wallet_balance = IF(user_wallet_balance-%1$s<=0,0,user_wallet_balance-%1$s) WHERE user_id = %2$s', secure($_POST['price']), secure($_POST['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
+                $query =   $db->query(sprintf('UPDATE users SET user_wallet_balance = IF(user_wallet_balance-%1$s<=0,0,user_wallet_balance-%1$s) WHERE user_id = %2$s', secure($_POST['price']), secure($check_user['id'], 'int'))) or _error("SQL_ERROR_THROWEN");
                 if($query == true){
                   /* log this transaction */
                   //  wallet_transaction_logs($_POST['id'], 'videohub_package_payment', 0, $_POST['price'], 'out');
 
-                  $db->query(sprintf("INSERT INTO ads_users_wallet_transactions (user_id, node_type, node_id, amount, type, date, platformType) VALUES (%s, %s, %s, %s, %s, %s, %s)", secure($_POST['id'], 'int'), secure('videohub_package_payment'), secure(0, 'int'), secure($_POST['price']), secure('out'),secure($date),secure('videohub'))) or _error("SQL_ERROR_THROWEN");
+                  $db->query(sprintf("INSERT INTO ads_users_wallet_transactions (user_id, node_type, node_id, amount, type, date, platformType) VALUES (%s, %s, %s, %s, %s, %s, %s)", secure($check_user['id'], 'int'), secure('videohub_package_payment'), secure(0, 'int'), secure($_POST['price']), secure('out'),secure($date),secure('videohub'))) or _error("SQL_ERROR_THROWEN");
 
                   returnResponse(true,200,"Success");
                 }else{
