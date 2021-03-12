c778b41df2aa40bf327375bdf76d6ecbb2d82118
Karan Kumar <karan.kumar@antiersolution.com>
2021-03-12 15:21:33 +0530

    Updated

--- includes/config.php
+++ includes/config.php
@@ -7,5 +7,5 @@ define("DB_PORT", "3306");
 define("SYS_URL", "https://www.stratus-stage.xyz");
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
-define("DEFAULT_LOCALE", "en_us");
+define("DEFAULT_LOCALE", "en_us"); 
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
