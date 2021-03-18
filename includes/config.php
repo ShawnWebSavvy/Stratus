63755eb412b843246234ec66202ab8a98a7d7bcf
Karan Kumar <karan.kumar@antiersolution.com>
2021-03-18 17:58:28 +0530

    Updated Code

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
-define("DB_NAME", "stratus");
+define("DB_NAME", "stratus_prod");
 define("DB_USER", "root");
-define("DB_PASSWORD", "Admin@123");
-define("DB_HOST", "localhost");
+define("DB_PASSWORD", "Passw0rd#123#!");
+define("DB_HOST", "127.0.0.1");
 define("DB_PORT", "3306");
-define("SYS_URL", "http://localhost/redis");
-define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
+define("SYS_URL", "https://stratus.co");
+define("API_BASE_URL", "https://ws.knoxglobal.com/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
