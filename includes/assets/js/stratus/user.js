fae5595bb93339388f1f3dce7a6691f248e3e619
Sargis <s.torosyan83@gmail.com>
2021-03-19 15:59:03 +0400

    fix add the post brick layer

--- includes/assets/js/stratus/user.js
+++ includes/assets/js/stratus/user.js
@@ -197,15 +197,17 @@ function data_heartbeat() {
                         var d = document.createElement('div');
                         d.innerHTML = values;
                         var valuesPost = d.firstChild;
+                      
                         if(bricklayer){
                            bricklayer.prepend(valuesPost);
                            bricklayer.redraw();
                         } else {
-                            $('.js_posts_stream').prepend('<div class="bricklayer"></div>');
-
                             var isClassExist = document.getElementsByClassName('bricklayer');
-                             if (isClassExist.length > 0) {
-                               var bricklayer = new Bricklayer(document.querySelector('.bricklayer'));
+                             if (!isClassExist.length) {
+                               $('.js_posts_stream').prepend('<div class="bricklayer"></div>');
+                                isClassExist = document.getElementsByClassName('bricklayer');
+                             }
+                               bricklayer = new Bricklayer(document.querySelector('.bricklayer'));
                                bricklayer.on("afterAppend", function (e) {
                                  var el = e.detail.item;
                                  el.classList.add('is-append');
@@ -215,8 +217,6 @@ function data_heartbeat() {
                                });
                                bricklayer.prepend(valuesPost);
                                bricklayer.redraw();
-                             }
-
                            
                         }
                     }
