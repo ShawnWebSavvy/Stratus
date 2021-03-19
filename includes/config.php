cd80dcb3085b157c522203c4cd50c37a6aac8b58
Karan Kumar <karan.kumar@antiersolution.com>
2021-03-19 14:41:30 +0530

    Udpated Bugs

--- includes/config.php
+++ includes/config.php
@@ -4,9 +4,10 @@ define("DB_USER", "root");
 define("DB_PASSWORD", "Passw0rd#123#!");
 define("DB_HOST", "127.0.0.1");
 define("DB_PORT", "3306");
-define("SYS_URL", "https://stratus.co");
+define("SYS_URL", "https://staging.stratus.co");
 //https://ws.stage-apollo.xyz/api
-define("API_BASE_URL", "https://ws.knoxglobal.com/api");
+//https://ws.knoxglobal.com/api
+define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
