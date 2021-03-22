9cfba1712cd75f4c478697817dba088d74bdb9f1
Karan Kumar <karan.kumar@antiersolution.com>
2021-03-22 17:46:59 +0530

    Code

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 define("DB_NAME", "stratus_prod");
 define("DB_USER", "root");
 define("DB_PASSWORD", "Admin@123");
-define("DB_HOST", "localhost");
+define("DB_HOST", "127.0.0.1");
 define("DB_PORT", "3306");
 define("SYS_URL", "https://stratus.co");
 //https://ws.stage-apollo.xyz/api
