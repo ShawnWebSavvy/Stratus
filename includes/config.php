5668b2f0878b5588ac26d941f8c1992769be408b
sourav.gupta <sourav.gupta@antechindia.in>
2021-03-24 00:25:40 +0530

    config changes

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
 define("DB_NAME", "sngine");
-define("DB_USER", "localhost");
+define("DB_USER", "root");
 define("DB_PASSWORD", "Admin@123");
 define("DB_HOST", "localhost");
 define("DB_PORT", "3306");
 define("SYS_URL", "http://localhost/stratusLive");
-define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
+define("API_BASE_URL", "https://ws.knoxglobal.com/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us"); 
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
