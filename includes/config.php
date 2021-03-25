b5352e8c971163f6fc207b0b438de3a2513c0ad0
kamaljit-antech <kamaljit.kaur@antechindia.com>
2021-03-25 12:47:06 +0530

    redis local

--- includes/config.php
+++ includes/config.php
@@ -2,15 +2,18 @@
 define("DB_NAME", "stratus");
 define("DB_USER", "root");
 define("DB_PASSWORD", "Admin@123");
-define("DB_HOST", "127.0.0.1");
+define("DB_HOST", "localhost");
 define("DB_PORT", "3306");
 define("SYS_URL", "http://localhost/stratus");
 //https://ws.stage-apollo.xyz/api
 //https://ws.knoxglobal.com/api 
-define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
+define("API_BASE_URL", "https://ws.knoxglobal.com/api");
+// define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us"); 
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
+define("PLY_URL", "http://localhost/playtube");
+define("PLAYTUBE_LINK", "http://localhost/playtube/logout");
 //FOR STAGE REPLACE https://cdn1.stratus.co with https://cdn.stratus-stage.xyz
 define("SYSTEM_UPLOADS", "https://cdn.stratus-stage.xyz/uploads");
 define("SYSTEM_UPLOADS_URL", "https://cdn.stratus-stage.xyz/uploads");
