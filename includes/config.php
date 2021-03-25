4b7dc00ca114d88f56abf1b536ad33fa4c298312
Karan Kumar <karan.kumar@antiersolution.com>
2021-03-25 18:18:45 +0530

    Updated issue

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,15 @@
 <?php
-define("DB_NAME", "stratus_stage");
-define("DB_USER", "root");
-define("DB_PASSWORD", "Passw0rd#123#!");
-define("DB_HOST", "127.0.0.1");
+define("DB_NAME", "notecloud"); 
+define("DB_USER", "notecloud");
+define("DB_PASSWORD", "LesinyyMVD4uqxKM");
+define("DB_HOST", "notecloud-rds.cc5yzmpks9xy.us-west-1.rds.amazonaws.com");
 define("DB_PORT", "3306");
-define("SYS_URL", "https://staging.stratus.co");
+define("SYS_URL", "https://www.stratus-stage.xyz");
 //https://ws.stage-apollo.xyz/api
 //https://ws.knoxglobal.com/api
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
-define("DEFAULT_LOCALE", "en_us");
+define("DEFAULT_LOCALE", "en_us"); 
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
 //FOR STAGE REPLACE https://cdn1.stratus.co with https://cdn.stratus-stage.xyz
 define("SYSTEM_UPLOADS", "https://cdn.stratus-stage.xyz/uploads");
