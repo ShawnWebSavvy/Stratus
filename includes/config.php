dffe324690a65006b370f564643c8973bec8fd0c
Karan Kumar <karan.kumar@antiersolution.com>
2021-03-26 09:46:14 +0530

    Upated Config

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 define("DB_NAME", "stratus");
 define("DB_USER", "root");
 define("DB_PASSWORD", "Admin@123");
-define("DB_HOST", "localhost");
+define("DB_HOST", "localhost"); 
 define("DB_PORT","3306");
 define("SYS_URL", "http://localhost/stratus");
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
