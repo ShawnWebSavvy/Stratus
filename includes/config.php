b5769e177df20e4b0cb36eaa32a0a75c2b231e80
Karan Kumar <karan.kumar@antiersolution.com>
2021-03-26 09:35:14 +0530

    Updated Code

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,17 @@
 <?php
-define("DB_NAME", "stratus_prod");
+define("DB_NAME", "stratus_stage");
 define("DB_USER", "root");
 define("DB_PASSWORD", "Passw0rd#123#!");
 define("DB_HOST", "127.0.0.1");
 define("DB_PORT", "3306");
-define("SYS_URL", "https://stratus.co");
+define("SYS_URL", "https://staging.stratus.co");
 //https://ws.stage-apollo.xyz/api
 //https://ws.knoxglobal.com/api
-define("API_BASE_URL", "https://ws.knoxglobal.com/api");
+define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
-define("DEFAULT_LOCALE", "en_us"); 
+define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
 //FOR STAGE REPLACE https://cdn1.stratus.co with https://cdn.stratus-stage.xyz
-define("SYSTEM_UPLOADS", "https://cdn1.stratus.co/uploads");
-define("SYSTEM_UPLOADS_URL", "https://cdn1.stratus.co/uploads");
-define("SYSTEM_UPLOADS_ASSETS", "https://cdn1.stratus.co"); 
+define("SYSTEM_UPLOADS", "https://cdn.stratus-stage.xyz/uploads");
+define("SYSTEM_UPLOADS_URL", "https://cdn.stratus-stage.xyz/uploads");
+define("SYSTEM_UPLOADS_ASSETS", "https://cdn.stratus-stage.xyz");
