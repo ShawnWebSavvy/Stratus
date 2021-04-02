4dbf5b2776704bba17abfc1c19cc533545cc23a1
Karan Kumar <karan.kumar@antiersolution.com>
2021-04-02 13:30:32 +0530

    ensuring updates

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ define("DB_USER", "notecloud");
 define("DB_PASSWORD", "LesinyyMVD4uqxKM");
 define("DB_HOST", "notecloud-rds.cc5yzmpks9xy.us-west-1.rds.amazonaws.com"); 
 define("DB_PORT","3306");
-define("SYS_URL", "https://www.stratus-stage.xyz/");
+define("SYS_URL", "https://www.stratus-stage.xyz");
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
