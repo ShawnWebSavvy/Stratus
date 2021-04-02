898dec679ed0352ca6d137fe97260ec0921b7e66
Karan Kumar <karan.kumar@antiersolution.com>
2021-04-02 13:12:32 +0530

    ensuring updates

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,14 @@
 <?php  
 define("DB_NAME", "notecloud");
-define("DB_USER", "root");
-define("DB_PASSWORD", "Admin@123");
-define("DB_HOST", "localhost"); 
+define("DB_USER", "notecloud");
+define("DB_PASSWORD", "LesinyyMVD4uqxKM");
+define("DB_HOST", "notecloud-rds.cc5yzmpks9xy.us-west-1.rds.amazonaws.com"); 
 define("DB_PORT","3306");
-define("SYS_URL", "http://localhost/stratus");
+define("SYS_URL", "https://www.stratus-stage.xyz/");
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "75i6TYjZy-3tymP-4jtPB-2Ulbr-375KZ-b6c299384b5a");
-define("PLY_URL", "http://localhost/playtube");
-define("PLAYTUBE_LINK", "http://localhost/playtube/logout");
+define("PLY_URL", "https://videohub.stratus-stage.xyz/");
+define("PLAYTUBE_LINK", "https://videohub.stratus-stage.xyz/logout");
 ?>
