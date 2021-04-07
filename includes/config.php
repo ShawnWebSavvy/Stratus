2da27184c7b60570461519f520e05fa1e52a8586
Karan Kumar <karan.kumar@antiersolution.com>
2021-04-07 10:00:53 +0530

    Notecloud

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,14 @@
 <?php
-define("DB_NAME", "sngine");
-define("DB_USER", "root");
-define("DB_PASSWORD", "Admin@123");
-define("DB_HOST", "localhost");
-define("DB_PORT", "3306");
-define("SYS_URL", "http://localhost/stratusNotecloud");
+define("DB_NAME", "notecloud");
+define("DB_USER", "notecloud");
+define("DB_PASSWORD", "LesinyyMVD4uqxKM");
+define("DB_HOST", "notecloud-rds.cc5yzmpks9xy.us-west-1.rds.amazonaws.com");
+define("DB_PORT","3306");
+define("SYS_URL", "https://www.stratus-stage.xyz");
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
-define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
-define("PLY_URL", "http://localhost/playtube");
-define("PLAYTUBE_LINK", "http://localhost/playtube/logout");
+define("LICENCE_KEY", "75i6TYjZy-3tymP-4jtPB-2Ulbr-375KZ-b6c299384b5a");
+define("PLY_URL", "https://videohub.stratus-stage.xyz/");
+define("PLAYTUBE_LINK", "https://videohub.stratus-stage.xyz/logout");
+?>
