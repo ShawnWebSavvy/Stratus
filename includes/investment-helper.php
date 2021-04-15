e6a98fbde3ab5a71ed1cb7d5173a2cef5e41be8e
sourav.gupta <sourav.gupta@antechindia.in>
2021-04-15 05:42:06 +0530

    Fixed investment buy sell issue

--- includes/investment-helper.php
+++ includes/investment-helper.php
@@ -94,7 +94,7 @@ class InvestmentHelper {
                 $order_id = $result['data']['data']['order_id'];
 
                 $db->query(sprintf("INSERT INTO investment_transactions (user_id, order_id, base_currency, tokens, currency, tnx_type ,amount, receive_amount, recieve_token, fees, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", secure($user_data['user_id'], 'int'),secure($order_id),secure('usd'), secure($token_value), secure($token_name), secure($action),secure($amount),secure($amount), secure($receive_token), secure($fees), secure('completed') ));
-                echo mysql_errno($db) . ": " . mysql_error($db) . "\n";die;
+                print_r($db);die;
                 $investment_id = $db->insert_id;
           
                 if($investment_id){
