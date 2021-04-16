b25394fd66424b8d7d34d2d0e6eab5bd6fad6981
Karan Kumar <karan.kumar@antiersolution.com>
2021-04-16 15:07:58 +0530

    Updated Branch

--- includes/config.php
+++ includes/config.php
@@ -1,18 +1,18 @@
 <?php
-define("DB_NAME", "notecloud");
-define("DB_USER", "notecloud");
-define("DB_PASSWORD", "LesinyyMVD4uqxKM");
-define("DB_HOST", "notecloud-rds.cc5yzmpks9xy.us-west-1.rds.amazonaws.com");
+define("DB_NAME", "stratus_latest");
+define("DB_USER", "root");
+define("DB_PASSWORD", "Admin@123");
+define("DB_HOST", "localhost");
 define("DB_PORT","3306");
-define("SYS_URL", "https://www.stratus-stage.xyz");
+define("SYS_URL", "http://localhost/stratus_merge");
 //https://ws.stage-apollo.xyz/api
 //https://ws.knoxglobal.com/api
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
 define("DEBUGGING", false);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "75i6TYjZy-3tymP-4jtPB-2Ulbr-375KZ-b6c299384b5a");
-define("PLY_URL", "https://videohub.stratus-stage.xyz/");
-define("PLAYTUBE_LINK", "https://videohub.stratus-stage.xyz/logout");
+define("PLY_URL", "http://localhost/playtube");
+define("PLAYTUBE_LINK", "http://localhost/playtube/logout");
 //FOR STAGE REPLACE https://cdn1.stratus.co with https://cdn.stratus-stage.xyz
 define("SYSTEM_UPLOADS", "https://cdn.stratus-stage.xyz/uploads");
 define("SYSTEM_UPLOADS_URL", "https://cdn.stratus-stage.xyz/uploads");
