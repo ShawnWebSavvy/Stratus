31536e021ec616da6cb6447eaa95f40f5f053b42
hardeep-antech <hardeep.singh@antechindia.com>
2021-04-26 10:08:07 +0530

    fix

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
-define("DB_NAME", "stratus_stage");
+define("DB_NAME", "investment");
 define("DB_USER", "root");
-define("DB_PASSWORD", "Passw0rd#123#!");
+define("DB_PASSWORD", "");
 define("DB_HOST", "127.0.0.1");
 define("DB_PORT", "3306");
-define("SYS_URL", "https://staging.stratus.co");
+define("SYS_URL", "http://localhost/stratus");
 //https://ws.stage-apollo.xyz/api
 //https://ws.knoxglobal.com/api
 define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
