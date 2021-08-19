e62b7a4c8256111a041c1197deac869f463048df
ShawnWebSavvy <53379212+ShawnWebSavvy@users.noreply.github.com>
2021-08-19 13:41:33 +0200

    Update config.php
    config update

--- includes/config.php
+++ includes/config.php
@@ -2,12 +2,13 @@
 define("DB_NAME", "stratus_stage_db");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
-define("DB_HOST", "localhost");
+//define("DB_PASSWORD", "Passw0rd#123#!");
+//define("DB_HOST", "127.0.0.1");
+define('DB_HOST', 'localhost');
 define("DB_PORT", "3306");
+//define("SYS_URL", "https://staging.stratus.co");
 define('SYS_URL', 'http://localhost/branch_stratus_shawn');
-//https://ws.stage-apollo.xyz/api
-//https://ws.knoxglobal.com/api
-define("API_BASE_URL", "https://ws.stage-apollo.xyz/api");
+define("API_BASE_URL", "https://apistaging.stratus.co");
 define("DEBUGGING", true);
 define("DEFAULT_LOCALE", "en_us");
 define("LICENCE_KEY", "2dZZzu6jS-3DMmA-2iBui-2OlV7-4zfJK-b6c299384b5a");
@@ -16,5 +17,6 @@ define("SYSTEM_UPLOADS", "https://cdn.stratus-stage.xyz/uploads");
 define("SYSTEM_UPLOADS_URL", "https://cdn.stratus-stage.xyz/uploads");
 define("SYSTEM_UPLOADS_ASSETS", "https://cdn.stratus-stage.xyz");
 define("PLY_URL", "https://videohubstage.stratus.co/");
+define("MRKT_URL", "https://markethub.stratus.co/");
 define("PLAYTUBE_LINK", "https://videohubstage.stratus.co/logout");
 ?>
